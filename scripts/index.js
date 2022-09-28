c0b5dafcd72555dd06ab07ccd5c48700220daa1a
Laktionova Ekaterina <ketrr1n@yandex.ru>
2022-09-28 15:52:28 +0300

    fix bugs

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,32 @@
-let formElement = document.querySelector('.popup');
+let popupEdit = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit');
 let closeButton = document.querySelector('.popup__close');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_who');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__who');
 
- editButton.addEventListener('click', function(e) {
-  e.preventDefault()
-  formElement.classList.add('popup_opened')
- });
+function openPopup (popup) {
+  popup.classList.add('popup_opened');
+}
+function closePopup (popup) {
+  popup.classList.remove('popup_opened');
+}
 
- closeButton.addEventListener('click', function() {
-  formElement.classList.remove('popup_opened');
- });
-
- let nameInput = document.querySelector('.popup__input_name');
- let jobInput = document.querySelector('.popup__input_who');
-
- function formSubmitHandler (evt) {
+function formSubmitHandler (evt) {
   evt.preventDefault();
-  console.log(nameInput.value);
-  console.log(jobInput.value);
-
-    let profileName = document.querySelector('.profile__name');
-    let profileJob = document.querySelector('.profile__who');
-   
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closePopup(popupEdit);
   }
 
   formElement.addEventListener('submit', formSubmitHandler);
+  editButton.addEventListener('click', () => {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+    openPopup(popupEdit);
+  });
+  closeButton.addEventListener('click', () => {
+    closePopup(popupEdit);
+  });
